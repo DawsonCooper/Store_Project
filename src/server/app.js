6b1cf4002eabc9d6d1e378bb2ef583a0e3f5ce6b
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2023-03-25 17:34:59 -0500

    Use the express.static middleware to connect static files to the ejs head

--- src/server/app.js
+++ src/server/app.js
@@ -1,10 +1,20 @@
 const express = require('express');
-
-
 const app = express();
+
+//const dbAccess = 'mongodb+srv://DawsonCooper:Allsnap36@store.fczgzcd.mongodb.net/?retryWrites=true&w=majority'
+
 app.set('view engine', 'ejs');
 app.listen(3000);
 
+
+
+app.use((req, res, next) => {
+    console.log('-----------------------------     New Request     -----------------------------------'); 
+    console.log(req.method);
+    console.log(req.path);
+    next();
+})
+app.use(express.static('static'))
 app.get('/', (req, res) => {
     res.render('index', {page: 'Home'});
 })
