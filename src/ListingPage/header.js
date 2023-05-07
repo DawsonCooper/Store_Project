135dd0c0716dd26015c8c3dbe5d59542848cc91d
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2023-05-07 10:02:27 -0500

    Added more header info and styles and animated the profile button onclick and hover

--- src/ListingPage/header.js
+++ src/ListingPage/header.js
@@ -16,10 +16,22 @@ export default function Header(props){
             </div>
             <div className='extra-content-row'>
                 <div className='extra-content-section-0'>
-                    <p className='review-count'>{listing.number_of_reviews} reviews </p>
+                    <span className='review-count'>{listing.number_of_reviews} reviews</span>
+                    {listing.host.host_is_superhost ? <span>Superhost</span> : null}
+                    <span className='bullet' />
+                    <a href={listing.host.host_url} target='_blank' rel="noreferrer" className='host-name'>{listing.host.host_name}</a>
+                    <span className='bullet' />
+                    <span className='location'>{listing.address.market}, {listing.address.country}</span>
                 </div>
                 <div className='extra-content-section-1'>
-
+                    <div className='favorite-wrapper'> 
+                        <span className='favorite-svg'>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="black" className="bi bi-heart" viewBox="0 0 16 16">
+                                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z"/>
+                            </svg>
+                        </span>
+                        <span className='save'>Save</span>
+                    </div>
                 </div>
             </div>
             
--- src/ListingPage/navbar.js
+++ src/ListingPage/navbar.js
@@ -23,7 +23,7 @@ export default function Navbar(props){
                     </svg>
                 </div>
             </div>
-            <div className='navbar-profile'>
+            <div className='navbar-profile listing-profile'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-list profile-menu" viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
             </svg>
